53ecd308cb7c6b41a24f164bc0fe77e81518e5f4
Natalie Triffo <nattriffo@gmail.com>
2022-07-13 18:27:02 -0500

    finishing routes

---
+++ routes/API/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  createUser,
+
+} = require('../../controllers/userController');
+
+
+router.route('/').get(getUsers).post(createUser);
+
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
