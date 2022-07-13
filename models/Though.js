89cdeed0d071f6e564a0125696c4844723b91b1f
Natalie Triffo <nattriffo@gmail.com>
2022-07-13 18:22:38 -0500

    finishing models folder. though.js

---
+++ models/Though.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
+
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Thought body is required',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      
+
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
